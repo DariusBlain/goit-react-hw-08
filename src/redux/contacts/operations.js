d07bef819d93b7734dce784b8ba0c0f34f196352
Serhii Syrov <surov.sergey.m@gmail.com>
2024-08-14 14:38:36 +0200

    nev back

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,11 @@
-import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
-axios.defaults.baseURL = "https://66b31db37fba54a5b7eb7749.mockapi.io/";
+import { goitApi } from "../../config/goitApi";
 
 export const fetchContactsThunk = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get("contacts");
+      const { data } = await goitApi.get("contacts");
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +17,7 @@ export const deleteContactThunk = createAsyncThunk(
   "contacts/deleteContact",
   async (id, thunkAPI) => {
     try {
-      await axios.delete(`contacts/${id}`);
+      await goitApi.delete(`contacts/${id}`);
       return id;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +29,7 @@ export const addContactThunk = createAsyncThunk(
   "contacts/addContact",
   async (body, thunkAPI) => {
     try {
-      const { data } = await axios.post("contacts", body);
+      const { data } = await goitApi.post("contacts", body);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
